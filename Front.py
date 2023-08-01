6b54d201131f3a041e1a87b09552bce15c3e45d5
salinasromerodaniel <42517271+salinasromerodaniel@users.noreply.github.com>
2023-08-01 11:09:27 -0600

    xsxs
    xsxsx

--- Front.py
+++ Front.py
@@ -1,8 +1,11 @@
 import mysql.connector
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask import g
 
 app = Flask(__name__)
+
+# Configuración para la sesión. Se necesita una clave secreta para firmar las cookies.
+app.secret_key = 'mi_clave_secreta'
 
 # Configura la conexión a la base de datos MySQL
 db_config = {
@@ -17,23 +20,43 @@ cursor = conn.cursor()
 
 # Simulación de usuarios y contraseñas (reemplaza esto con una base de datos en un entorno de producción)
 users = {
-    'admin': 'admin',
+    'admin': 'admin'
 }
 
 @app.route('/')
 def index():
     return render_template('login.html')
-
-@app.route('/login', methods=['POST'])
+    
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    if username in users and users[username] == password:
-        # Aquí puedes agregar código para iniciar sesión, como establecer una sesión de usuario.
-        return f'¡Bienvenido, {username}!'
+        if username in users and users[username] == password:
+            # Inicio de sesión exitoso, almacenamos el estado de inicio de sesión en la sesión del usuario.
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('menuP'))
+        else:
+            return 'Credenciales inválidas. Por favor, vuelve a intentarlo.'
     else:
-        return 'Credenciales inválidas. Por favor, vuelve a intentarlo.'
+        return render_template('login.html')
+    
+@app.route('/menuP')
+def menuP():
+    # Comprobamos si el usuario está logeado. Si no, lo redirigimos al inicio de sesión.
+    if 'logged_in' in session and session['logged_in']:
+        # El usuario está logeado, renderizamos la página con el menú.
+        return render_template('MenuPrincipal.html')
+    else:
+        return redirect(url_for('logout'))
+    
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    # Cerramos la sesión del usuario limpiando los datos de la sesión.
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__=='__main__':
     app.run(debug=True, port=5000)
