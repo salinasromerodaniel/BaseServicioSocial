a8bb726cff4ccb58215416f689c149f7da975377
salinasromerodaniel <42517271+salinasromerodaniel@users.noreply.github.com>
2023-08-01 09:18:30 -0600

    sad
    asda

--- Front.py
+++ Front.py
@@ -2,12 +2,21 @@ import mysql.connector
 from flask import Flask, render_template, request
 from flask import g
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="admin"
-)
-
 app = Flask(__name__)
 
-print(mydb)
+# Configura la conexión a la base de datos MySQL
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'admin',
+    'database': 'Inventario',
+}
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
